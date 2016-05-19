96e0e44b664eb30cedca7bd63b890cb9481222c8
ascoelho <anthonystefancoelho@gmail.com>
2016-05-19 10:34:45 -0400

    first commit

---
+++ Cover Flow Layout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cover Flow Layout
+//
+//  Created by Anthony Coelho on 2016-05-19.
+//  Copyright © 2016 Anthony Coelho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cover Flow Layout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cover Flow Layout
+//
+//  Created by Anthony Coelho on 2016-05-19.
+//  Copyright © 2016 Anthony Coelho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
